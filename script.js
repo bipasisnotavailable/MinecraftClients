4e95c1c3b794933c46de3d201acb83ba6566cf16
Léo <101277451+bipasisnotavailable@users.noreply.github.com>
2024-08-18 00:39:07 +0200

    Add files via upload

--- script.js
+++ script.js
@@ -68,7 +68,7 @@ const clients = [
         description: "DoomsDay is a multifunctional modification for Minecraft with many useful utilities. (like ghost client) Supports almost all versions of Minecraft!",
         cover: "https://assets-global.website-files.com/662ff817ffae75d95d2a93e4/662ffa85ecf13e98abc98ea9_Bez%20tytu%C5%82u.png",
         screenshots: ["https://doomsdayclient.com/doomsday.png"],
-        downloadLink: "https://lootdest.org/s?37eb7f96",
+        downloadLink: "https://doomsdayclient.com/",
         colors: {
             text: "#000000", // Text
             accent: "#b0b0b0", // Accent
