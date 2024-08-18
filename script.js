c6317e8aedfa84cb0c12bf63f877a63b6c758ad2
Léo <101277451+bipasisnotavailable@users.noreply.github.com>
2024-08-19 00:01:26 +0200

    Add files via upload

--- script.js
+++ script.js
@@ -68,7 +68,7 @@ const clients = [
         description: "DoomsDay is a multifunctional modification for Minecraft with many useful utilities. (like ghost client) Supports almost all versions of Minecraft!",
         cover: "https://assets-global.website-files.com/662ff817ffae75d95d2a93e4/662ffa85ecf13e98abc98ea9_Bez%20tytu%C5%82u.png",
         screenshots: ["https://doomsdayclient.com/doomsday.png"],
-        downloadLink: "https://doomsdayclient.com/",
+        downloadLink: 'https://lootdest.org/s?1f578b31',
         colors: {
             text: "#000000", // Text
             accent: "#b0b0b0", // Accent
@@ -83,7 +83,7 @@ const clients = [
         description: "Cheat for Pixel Gun 3D PC",
         cover: "https://fbi.cults3d.com/uploaders/16600790/illustration-file/16a0b6ea-d282-444a-b222-5853ccd49e35/IMG_0316.webp",
         screenshots: ["https://i.ytimg.com/vi/DCFoMVurCV0/maxresdefault.jpg"],
-        downloadLink: "https://discord.gg/qfgMJj5KF4",
+        downloadLink: "https://lootdest.org/s?d566af46",
         colors: {
             text: "#000000", // Text
             accent: "#b0b0b0", // Accent
@@ -98,7 +98,7 @@ const clients = [
         description: "FDP is a hacked client (also known as a cheat) for Minecraft and designed to give you a significant advantage over other players. Unlike many of its competitors, FDP is completely free, open source and compatible with Forge. FDP is a fork of liquidbounce so you get the benefits of liquidbounce while still getting updates and support.",
         cover: "https://user-images.githubusercontent.com/105464811/177683437-8b768dcf-0e15-4ac4-85f0-2b673c2e8540.png",
         screenshots: ["https://www.9minecraft.net/wp-content/uploads/2022/08/FDP-Client-Screenshots-2.jpg", "https://www.9minecraft.net/wp-content/uploads/2022/08/FDP-Client-Screenshots-9.jpg"],
-        downloadLink: "https://fdpinfo.github.io/download",
+        downloadLink: "https://lootdest.org/s?db52142d",
         colors: {
             text: "#000000", // Text
             accent: "#df5eff", // Accent
@@ -113,7 +113,7 @@ const clients = [
         description: "Easy to use, powerful, and frequently updated. Millions of downloads and counting. Aristois offers over 100 in-game modules to extend and enhance your Minecraft gameplay. With a focus on PvE, we offer a modern and personlised experience — get it today! In addition to many unique modules and features, Aristois contains a worldwide in-game chat. Plan your next adventure and coordinate with players on other servers, right from the chat. The Aristois UI is designed with customisation in mind, so it can look exactly how you want it. We've also created two additional UIs to enhance speed and smoothness of enabling mods.",
         cover: "https://aristois.net/logo.webp",
         screenshots: ["https://aristois.net/features.webp"],
-        downloadLink: "https://aristois.net",
+        downloadLink: "https://lootdest.org/s?1384c193'",
         colors: {
             text: "#000000", // Text
             accent: "#85a9ff", // Accent
@@ -128,7 +128,7 @@ const clients = [
         description: "Based Minecraft utility mod.",
         cover: "https://meteorclient.com/icon.png",
         screenshots: ["https://www.9minecraft.net/wp-content/uploads/2022/06/Meteor-Client-Mod-Screenshots-10.jpg"],
-        downloadLink: "https://meteorclient.com",
+        downloadLink: "https://lootdest.org/s?6912fd22",
         colors: {
             text: "#000000", // Text
             accent: "#9000ff", // Accent
@@ -159,6 +159,186 @@ const clients = [
         cover: "https://media.discordapp.net/attachments/1223650631060033649/1274749678327566386/png.png?ex=66c362a7&is=66c21127&hm=fe0de67b678d837b1b22ea3dc4b0f9c62e35c7b7082c89ea6400a4f9bb823bb7&=&format=webp&quality=lossless&width=676&height=676",
         screenshots: ["https://i.ytimg.com/vi/eVo6Oc7KGDY/hq720.jpg?sqp=-oaymwEhCK4FEIIDSFryq4qpAxMIARUAAAAAGAElAADIQj0AgKJD&rs=AOn4CLAH0JbEZdTEI91oMZlcdMdle68_lw"],
         downloadLink: "https://discord.com/invite/packetv3",
+        colors: {
+            text: "#ffffff", // Text
+            accent: "#00bf2d", // Accent
+            header: "#009fbf" // Header
+        }
+    },
+    {
+        id: 12,
+        name: "Raven B4",
+        price: "$150",
+        tags: ["Hypixel", "Forge"],
+        description: "25$ discount when you buy trough 'ihasedich'",
+        cover: "https://i.pinimg.com/736x/28/bc/1d/28bc1d0c2420d7cea6238b1200b3b35f.jpg",
+        screenshots: [],
+        downloadLink: "ihasedich",
+        colors: {
+            text: "#ffffff", // Text
+            accent: "#00bf2d", // Accent
+            header: "#009fbf" // Header
+        }
+    },
+    {
+        id: 13,
+        name: 'Wurst',
+        price: 'Free',
+        tags: [],
+        description: "OP Client",
+        cover: 'https://wurst.wiki/_media/logo/wurst_centered_256.png',
+        screenshots: [],
+        downloadLink: 'https://lootdest.org/s?357de362',
+        colors: {
+            text: "#ffffff", // Text
+            accent: "#00bf2d", // Accent
+            header: "#009fbf" // Header
+        }
+    },
+    {
+        id: 200,
+        name: 'tkazer',
+        price: 'Free',
+        tags: ['CS2', 'Free'],
+        description: "CS2 Cheat",
+        cover: 'https://de.exloader.net/resources/favicon.png',
+        screenshots: [],
+        downloadLink: 'https://lootdest.org/s?bcea7495',
+        colors: {
+            text: "#ffffff", // Text
+            accent: "#00bf2d", // Accent
+            header: "#009fbf" // Header
+        }
+    },
+    {
+        id: 201,
+        name: 'aimstar',
+        price: 'Free',
+        tags: ['CS2', 'Free'],
+        description: "CS2 Cheat",
+        cover: 'https://de.exloader.net/resources/favicon.png',
+        screenshots: [],
+        downloadLink: 'https://lootdest.org/s?bcea7495',
+        colors: {
+            text: "#ffffff", // Text
+            accent: "#00bf2d", // Accent
+            header: "#009fbf" // Header
+        }
+    },
+    {
+        id: 202,
+        name: 'En1gma',
+        price: 'Free',
+        tags: ['CS2', 'Free'],
+        description: "CS2 Cheat",
+        cover: 'https://de.exloader.net/resources/favicon.png',
+        screenshots: [],
+        downloadLink: 'https://loot-link.com/s?f0f7465a',
+        colors: {
+            text: "#ffffff", // Text
+            accent: "#00bf2d", // Accent
+            header: "#009fbf" // Header
+        }
+    },
+    {
+        id: 203,
+        name: 'Ext. XONE',
+        price: 'Free',
+        tags: ['CS2', 'Free'],
+        description: "CS2 Cheat",
+        cover: 'https://de.exloader.net/resources/favicon.png',
+        screenshots: [],
+        downloadLink: 'https://lootdest.org/s?f263b49a',
+        colors: {
+            text: "#ffffff", // Text
+            accent: "#00bf2d", // Accent
+            header: "#009fbf" // Header
+        }
+    },
+    {
+        id: 204,
+        name: 'Osiris v2',
+        price: 'Free',
+        tags: ['CS2', 'Free'],
+        description: "CS2 Cheat",
+        cover: 'https://de.exloader.net/resources/favicon.png',
+        screenshots: [],
+        downloadLink: 'https://loot-link.com/s?63d9c626',
+        colors: {
+            text: "#ffffff", // Text
+            accent: "#00bf2d", // Accent
+            header: "#009fbf" // Header
+        }
+    },
+    {
+        id: 205,
+        name: 'Asphyxia',
+        price: 'Free',
+        tags: ['CS2', 'Free'],
+        description: "CS2 Cheat",
+        cover: 'https://de.exloader.net/resources/favicon.png',
+        screenshots: [],
+        downloadLink: 'https://loot-link.com/s?212500ed',
+        colors: {
+            text: "#ffffff", // Text
+            accent: "#00bf2d", // Accent
+            header: "#009fbf" // Header
+        }
+    },
+    {
+        id: 206,
+        name: 'IMXNOOBX',
+        price: 'Free',
+        tags: ['CS2', 'Free'],
+        description: "CS2 Cheat",
+        cover: 'https://de.exloader.net/resources/favicon.png',
+        screenshots: [],
+        downloadLink: 'https://lootdest.org/s?34f3fdd0',
+        colors: {
+            text: "#ffffff", // Text
+            accent: "#00bf2d", // Accent
+            header: "#009fbf" // Header
+        }
+    },
+    {
+        id: 207,
+        name: 'Orbit',
+        price: 'Free',
+        tags: ['CS2', 'Free'],
+        description: "CS2 Cheat",
+        cover: 'https://de.exloader.net/resources/favicon.png',
+        screenshots: [],
+        downloadLink: 'https://lootdest.org/s?ea144cac',
+        colors: {
+            text: "#ffffff", // Text
+            accent: "#00bf2d", // Accent
+            header: "#009fbf" // Header
+        }
+    },
+    {
+        id: 208,
+        name: 'Plaguecheat.cc',
+        price: 'Free',
+        tags: ['CS2', 'Free'],
+        description: "CS2 Cheat",
+        cover: 'https://de.exloader.net/resources/favicon.png',
+        screenshots: [],
+        downloadLink: 'https://lootdest.org/s?ba5ed5c6',
+        colors: {
+            text: "#ffffff", // Text
+            accent: "#00bf2d", // Accent
+            header: "#009fbf" // Header
+        }
+    },
+    {
+        id: 209,
+        name: 'SDK2Changer',
+        price: 'Free',
+        tags: ['CS2', 'Free'],
+        description: "CS2 Cheat",
+        cover: 'https://de.exloader.net/resources/favicon.png',
+        screenshots: [],
+        downloadLink: 'https://lootdest.org/s?9be4491b',
         colors: {
             text: "#ffffff", // Text
             accent: "#00bf2d", // Accent
